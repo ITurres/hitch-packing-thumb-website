8f271289a0ca1d441e41b842c606e3cd1512ba25
ITurres <emanueliturres.co@gmail.com>
2023-03-09 12:40:01 -0300

    Rename all -navbar- related class selectors

--- src/components/markup-templates/navbar-li-items-template.js
+++ src/components/markup-templates/navbar-li-items-template.js
@@ -1,7 +1,7 @@
 export const navbarLiItemTemplate = (htmlPage, faClass, spanText) => {
-  return `<li class="navbar__li">
-<a class="navbar__link" href="${htmlPage}"
+  return `<li class="my-navbar__li">
+<a class="my-navbar__link" href="${htmlPage}"
   ><i class="${faClass}"></i>
-  <span class="navbar__item">${spanText}</span></a>
+  <span class="my-navbar__item">${spanText}</span></a>
 </li>`;
 };
